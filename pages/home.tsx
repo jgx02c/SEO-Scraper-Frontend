ff8c1081efd407301e7ecc8fee31d8913fbb3f0e
Joshua <97928637+jgx02c@users.noreply.github.com>
2025-02-06 13:31:27 -0800

    updates

--- pages/home.tsx
+++ pages/home.tsx
@@ -1,16 +1,9 @@
 import Image from 'next/image';
 import Profile from '@/public/dialogica_logo.png';
 import Settings from '@/public/Settings.svg';
-// import { Insight } from "@/components/insight/insight"
-// import { InsightExpanded } from "@/components/insightexpanded/insightexpanded"
-import React, { useEffect, useState } from 'react';
+import React, { useState } from 'react';
 import classNames from 'classnames';
-import styles from "./index.module.scss";
-// import { TimeTrack } from "@/components/timetrack/timetrack";
-import { InsightDataType, TimeTrackDataType } from '@/types/types';
-// import { MainSetting } from '@/components/mainsettings/mainsettings';
-
-import { useIP } from '@/app/IPProvider';
+import styles from './index.module.scss';
 import { NavHeader } from '@/webComponents/mainnav';
 import { Main } from '@/webComponents/main';
 import { Reputation } from '@/webComponents/reputation';
@@ -19,21 +12,47 @@ import { Featuretwo } from '@/webComponents/featureTwo';
 import { Footer } from '@/webComponents/footer';
 import { Join } from '@/webComponents/join';
 
-
 interface MainProps {
     className?: string;
 }
-  
-export const Home: React.FC<MainProps> = ({ className }) => {
 
-  
-return(
-    <div className={classNames(styles.root)}>
-        <NavHeader />
-        <Main />
-        
-    </div>
- );
+export const Home: React.FC<MainProps> = ({ className }) => {
+    const [activeComponent, setActiveComponent] = useState<'myCompany' | 'companies' | 'addCompany' | 'chat'>('myCompany');
+    
+    const renderComponent = () => {
+        switch (activeComponent) {
+            case 'myCompany':
+                return <Chat />;
+            case 'companies':
+                return <Companies />;
+            case 'addCompany':
+                return <MyCompany />;
+            case 'chat':
+                return <Settings />;
+            default:
+                return <Main />;
+        }
+    };
+    
+    return (
+        <div className={classNames(styles.root)}>
+            <div className={styles.sidebar}>
+                <div className={styles.profileSection}>
+                    <Image src={Profile} alt="Profile" width={50} height={50} />
+                </div>
+                <ul className={styles.menu}>
+                    <li onClick={() => setActiveComponent('myCompany')}>My Company</li>
+                    <li onClick={() => setActiveComponent('companies')}>Companies</li>
+                    <li onClick={() => setActiveComponent('addCompany')}>Add Company</li>
+                    <li onClick={() => setActiveComponent('chat')}>Chat</li>
+                </ul>
+            </div>
+            <div className={styles.mainContent}>
+                <NavHeader />
+                {renderComponent()}
+            </div>
+        </div>
+    );
 }
 
 export default Home;
