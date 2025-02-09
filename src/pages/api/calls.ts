ae7eb1d63c7a32e3a5b8d91c30e43f95a4f84566
Joshua <97928637+jgx02c@users.noreply.github.com>
2025-02-09 11:40:00 -0800

    Update calls.ts

--- src/pages/api/calls.ts
+++ src/pages/api/calls.ts
@@ -1,3 +1,21 @@
+// Next.js API route support: https://nextjs.org/docs/api-routes/introduction// Ok so you need to add a config file for the URL base structure `{?urlConfig}/(call to be made)`
+// When you call the "my business tab" it will call the same competitors function but with the business_id set to 1
+// Slim down the fast api server to remove the BS that is there (extra nonsense not being used)
+
+// Overview
+
+// Call to server for get_business_by_id
+
+// Call to server to get_all_business  
+
+
+//Websocket for Chat
+
+// Calls to server to for Chat Bot Websocket.
+// Look at the code for it to make it work.
+
+
+
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import type { NextApiRequest, NextApiResponse } from "next";
 
