65b5f9a507d68ff458015b64e01806cf74eb9306
Joshua <97928637+jgx02c@users.noreply.github.com>
2025-02-18 11:03:37 -0800

    changed URL's

--- src/pages/api/onboardingAPI.ts
+++ src/pages/api/onboardingAPI.ts
@@ -15,7 +15,7 @@ interface SubmitWebsiteResponse {
         throw new Error('No authentication token found');
       }
   
-      const response = await fetch(`http://127.0.0.1:8000/api/website/analyze`, {
+      const response = await fetch(`https://leaps-scraper.onrender.com/api/website/analyze`, {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
